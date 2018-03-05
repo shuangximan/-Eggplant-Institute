aa57912b8bd865cddb1a712f309d60744a7294e4
Hsuan <xyj90@qq.com>
2018-03-06 02:52:49 +0800

    茄子研究所小程序1.0
    茄子研究所小程序1.0

---
+++ pages/logs/logs.js
@@ -0,0 +1,25 @@
+/*
+ * 
+ * WordPres版微信小程序
+ * author: jianbo
+ * organization: 茄子研究所  q-z.top
+ * github:    https://github.com/iamxjb/winxin-app-watch-life.net
+ * 技术支持微信号：iamxjb
+ * 开源协议：MIT
+ * Copyright (c) 2017 https://q-z.top All rights reserved.
+ * 
+ */
+
+var util = require('../../utils/util.js')
+Page({
+  data: {
+    logs: []
+  },
+  onLoad: function () {
+    this.setData({
+      logs: (wx.getStorageSync('logs') || []).map(function (log) {
+        return util.formatTime(new Date(log))
+      })
+    })
+  }
+})
